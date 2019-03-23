9d0f6bd7fe9e7d481273d90c4e1d476cc51186f5
Miguel Yax <mig_dj@hotmail.com>
2019-03-22 20:03:14 -0600

    tiempo de checkeo y archivo para iniciar el servicio de node

---
+++ run.sh
@@ -0,0 +1,37 @@
+##------------------------------------------------------------------
+##CREAR ARCHIVO EN CONSOLA 
+## nano run.sh
+## copiar el contenido a la consola
+## ctrl+o
+## enter
+## ctrl+x
+##------------------------------------------------------------------
+
+
+## RUN APP
+sudo apt install curl
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+
+sudo apt install nodejs
+
+sudo npm install forever -g
+
+## INSTALL GIT
+sudo dnf install git-all
+
+sudo apt install git-all
+
+## DOWNLOAD PROYECT
+
+git clone https://github.com/MiguelYax/image-warehouse.git
+
+cd image-warehouse
+
+## instalar dependencias
+
+npm install 
+
+## ejecutar servicio como demoño
+
+forever start bin/www
